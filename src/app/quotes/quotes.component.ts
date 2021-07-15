514f42fb8f247ca476f850e0bfc82b1b16f9ed37
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-15 21:32:49 +0300

    form input 1

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuotesTemplate } from '../quotes-template'
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  mouseHovering(index:number){
+    this.quotes[index].isHidden = !this.quotes[index].isHidden;
+  }
+  mouseLeft(index:number){
+    this.quotes[index].isHidden = false;
+  }
+
+  quotes: QuotesTemplate[] = [
+    new QuotesTemplate(1, 'Watch finding Nemo', 0),
+    new QuotesTemplate(2, 'Watch finding Nemo', 0),
+    new QuotesTemplate(3, 'Watch finding Nemo', 0),
+    new QuotesTemplate(4, 'Watch finding Nemo', 0),
+    new QuotesTemplate(5, 'Watch finding Nemo', 0),
+    new QuotesTemplate(6, 'Watch finding Nemo', 0)
+  ];
+
+  plusOne(positiveVote:boolean, index:number){
+    if(positiveVote){
+      this.quotes[index].votes ++
+    }
+  }
+  minusOne(negativeVote:boolean, index:number){
+    if(negativeVote){
+      this.quotes[index].votes --
+    }
+  }
+  removeQuote(deleteQuote:boolean, index:number){
+    if(deleteQuote){
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].author}?`) //Backticks have been used
+    }
+    if (deleteQuote){
+      this.quotes.splice(index, 1)
+    }
+  }
 
   constructor() { }
 
