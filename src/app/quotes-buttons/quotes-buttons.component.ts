3c7c73a3812058301219a729c58ecdc217a133ba
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-15 21:33:34 +0300

    vote counter initialized from 0

---
+++ src/app/quotes-buttons/quotes-buttons.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { QuotesTemplate } from '../quotes-template'
+
+@Component({
+  selector: 'app-quotes-buttons',
+  templateUrl: './quotes-buttons.component.html',
+  styleUrls: ['./quotes-buttons.component.css']
+})
+export class QuotesButtonsComponent implements OnInit {
+
+  @Input() showHideButtons!: QuotesTemplate;
+  @Output() positiveVote = new EventEmitter <boolean>();
+  @Output() negativeVote = new EventEmitter <boolean>();
+  @Output() deleteQuote = new EventEmitter <boolean>();
+
+  incrementUp(positive:boolean){
+    this.positiveVote.emit(positive);
+  }
+
+  incrementDown(negative:boolean){
+    this.negativeVote.emit(negative);
+  }
+
+  quoteDeletion(remove:boolean){
+    this.deleteQuote.emit(remove);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
