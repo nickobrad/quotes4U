0d6a09deb20ecd7c74df81c7549d091ba9ddfe7d
Nicholas Bradley <nicholas.omondi@student.moringaschool.com>
2021-07-15 22:45:20 +0300

    quote template modified

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { QuotesTemplate } from '../quotes-template'
+
 
 @Component({
   selector: 'app-quotes-form',
   templateUrl: './quotes-form.component.html',
   styleUrls: ['./quotes-form.component.css']
 })
+
 export class QuotesFormComponent implements OnInit {
 
+  @Output() addQuote = new EventEmitter<QuotesTemplate>();
+  newQuote = new QuotesTemplate(0, "", "", new Date(), 0, 0);
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit(): void {
